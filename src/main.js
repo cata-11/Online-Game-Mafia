0b465ffb7794769ab445654abb2e35aa7ce17f8e
raducu999 <radustefan1302@gmail.com>
2020-12-23 20:17:00 +0200

    probleme

--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import store from "./store";
-import VueCountdownTimer from 'vuejs-countdown-timer';
+import VueCountdownTimer from "vuejs-countdown-timer";
+
+import socketio from "socket.io";
+import VueSocketIO from "vue-socket.io";
+
+export const SocketInstance = socketio("http://localhost:4113");
+
+Vue.use(VueSocketIO, SocketInstance);
 Vue.use(VueCountdownTimer);
 Vue.config.productionTip = false;
 
